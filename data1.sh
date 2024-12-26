68b15e594534393a3b328a223d05741b166300ef
Pravalika-27 <112800726+Pravalika-27@users.noreply.github.com>
2024-12-26 13:54:57 +0530

    Delete data1.sh

--- data1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update packages and install Apache and Git
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-yum install git -y
-sudo git clone https://github.com/Pravalika-27/project.git
-sudo mv project/html/* /var/www/html/
